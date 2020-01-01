59b429578e569058b3a1db576e3e0b48eb9508b7
SeasideLee <liseaside@163.com>
2020-01-01 17:48:53 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './App.css'
-import { UserSession } from 'blockstack'
+import {UserSession} from 'blockstack'
 
 import Landing from './Landing'
 import SignedIn from './SignedIn'
 
+import Proxy from './setupProxy'
+
 class App extends Component {
 
-  constructor() {
-    super()
-    this.userSession = new UserSession()
-  }
-
-  componentWillMount() {
-    const session = this.userSession
-    if(!session.isUserSignedIn() && session.isSignInPending()) {
-      session.handlePendingSignIn()
-      .then((userData) => {
-        if(!userData.username) {
-          throw new Error('This app requires a username.')
-        }
-        window.location = `/kingdom/${userData.username}`
-      })
+    constructor() {
+        super()
+        this.userSession = new UserSession()
     }
-  }
 
-  render() {
-    return (
-      <main role="main">
-          {this.userSession.isUserSignedIn() ?
-            <SignedIn />
-          :
-            <Landing />
-          }
-      </main>
-    );
-  }
+    componentWillMount() {
+        if (process.env.NODE_ENV !== 'development') {
+            Proxy(this);
+        }
+        const session = this.userSession
+        if (!session.isUserSignedIn() && session.isSignInPending()) {
+            session.handlePendingSignIn()
+                .then((userData) => {
+                    if (!userData.username) {
+                        throw new Error('This app requires a username.')
+                    }
+                    window.location = `/kingdom/${userData.username}`
+                })
+        }
+    }
+
+    render() {
+        return (
+            <main role="main">
+                {this.userSession.isUserSignedIn() ?
+                    <SignedIn/>
+                    :
+                    <Landing/>
+                }
+            </main>
+        );
+    }
 }
 
 export default App
